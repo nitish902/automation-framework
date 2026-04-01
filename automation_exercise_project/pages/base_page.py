4fbb7cdb44c27845a4b24b98c592f2efed97007d
nitish902 <nitishshukla902@gmail.com>
2026-04-01 17:47:39 +0530

    Update base_page.py

--- automation_exercise_project/pages/base_page.py
+++ automation_exercise_project/pages/base_page.py
@@ -6,32 +6,69 @@ from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
 
     def __init__(self, driver):
+
         self.driver = driver
+
+        self.wait = WebDriverWait(driver, 15)
 
 
     def find(self, xpath):
-        return self.driver.find_element(By.XPATH, xpath)
+
+        return self.wait.until(
+
+            EC.presence_of_element_located((By.XPATH, xpath))
+
+        )
 
 
     def finds(self, xpath):
-        return self.driver.find_elements(By.XPATH, xpath)
+
+        return self.wait.until(
+
+            EC.presence_of_all_elements_located((By.XPATH, xpath))
+
+        )
 
 
     def click(self, xpath):
-        self.find(xpath).click()
+
+        element = self.wait.until(
+
+            EC.element_to_be_clickable((By.XPATH, xpath))
+
+        )
+
+        element.click()
 
 
     def type(self, xpath, text):
-        element = self.find(xpath)
+
+        element = self.wait.until(
+
+            EC.visibility_of_element_located((By.XPATH, xpath))
+
+        )
+
         element.clear()
+
         element.send_keys(text)
 
 
     def get_text(self, xpath):
-        return self.find(xpath).text
 
+        element = self.wait.until(
 
-    def wait_for(self, xpath, time=10):
-        WebDriverWait(self.driver, time).until(
             EC.visibility_of_element_located((By.XPATH, xpath))
+
+        )
+
+        return element.text
+
+
+    def wait_for(self, xpath):
+
+        self.wait.until(
+
+            EC.visibility_of_element_located((By.XPATH, xpath))
+
         )

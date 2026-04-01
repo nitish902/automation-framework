d31810cd6619247a0ba05d7c93c54268c4a37656
nitish902 <nitishshukla902@gmail.com>
2026-04-01 16:25:11 +0530

    Update conftest.py

--- automation_exercise_project/conftest.py
+++ automation_exercise_project/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from config.config_reader import get_config
-import os
-from datetime import datetime
 
 
 @pytest.fixture
@@ -13,39 +13,20 @@ def driver():
     browser = config["browser"]
 
     if browser == "chrome":
-        driver = webdriver.Chrome()
 
-    elif browser == "edge":
-        driver = webdriver.Edge()
+        service = Service(ChromeDriverManager().install())
+
+        driver = webdriver.Chrome(service=service)
 
     else:
-        driver = webdriver.Chrome()
+
+        service = Service(ChromeDriverManager().install())
+
+        driver = webdriver.Chrome(service=service)
+
 
     driver.maximize_window()
 
     yield driver
 
     driver.quit()
-
-
-# screenshot hook
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item):
-
-    outcome = yield
-
-    report = outcome.get_result()
-
-    if report.when == "call" and report.failed:
-
-        driver = item.funcargs.get("driver")
-
-        if driver:
-
-            os.makedirs("screenshots", exist_ok=True)
-
-            file_name = f"screenshots/{item.name}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"
-
-            driver.save_screenshot(file_name)
-
-            print(f"\nScreenshot saved: {file_name}")

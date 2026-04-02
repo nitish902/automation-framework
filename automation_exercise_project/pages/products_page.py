baa0542fc87829dfae01152643b59caf45a0c4a5
nitish902 <nitishshukla902@gmail.com>
2026-04-02 10:46:30 +0530

    Update products_page.py

--- automation_exercise_project/pages/products_page.py
+++ automation_exercise_project/pages/products_page.py
@@ -1,7 +1,10 @@
 from pages.base_page import BasePage
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class ProductsPage(BasePage):
+
+    # locators
 
     PRODUCTS_TITLE = "//h2[text()='All Products']"
 
@@ -9,7 +12,7 @@ class ProductsPage(BasePage):
 
     PRODUCT_NAMES = "//div[@class='productinfo text-center']/p"
 
-    ADD_TO_CART_FIRST = "(//a[contains(text(),'Add to cart')])[1]"
+    ADD_TO_CART_BTNS = "//a[contains(text(),'Add to cart')]"
 
     VIEW_CART_BTN = "//u[text()='View Cart']"
 
@@ -18,32 +21,91 @@ class ProductsPage(BasePage):
     SEARCH_BTN = "//button[@id='submit_search']"
 
 
+
+    # page loaded validation
+
     def is_products_page_loaded(self):
-        self.wait_for(self.PRODUCTS_TITLE)
+
         return self.find(self.PRODUCTS_TITLE).is_displayed()
 
 
+
+    # count products
+
     def get_product_count(self):
-        self.wait_for(self.PRODUCT_CARDS)
+
         return len(self.finds(self.PRODUCT_CARDS))
 
 
+
+    # get all product names
+
     def get_all_product_names(self):
+
         elements = self.finds(self.PRODUCT_NAMES)
+
         return [e.text for e in elements]
 
 
-    def add_first_product_to_cart(self):
-        self.wait_for(self.ADD_TO_CART_FIRST)
-        self.click(self.ADD_TO_CART_FIRST)
 
-
-    def view_cart(self):
-        self.wait_for(self.VIEW_CART_BTN)
-        self.click(self.VIEW_CART_BTN)
-
+    # search product
 
     def search_product(self, product_name):
-        self.wait_for(self.SEARCH_INPUT)
+
         self.type(self.SEARCH_INPUT, product_name)
+
         self.click(self.SEARCH_BTN)
+
+
+
+    # add first product to cart (CI stable)
+
+    def add_first_product_to_cart(self):
+
+        products = self.finds(self.PRODUCT_CARDS)
+
+        first_product = products[0]
+
+
+
+        # hover on product
+
+        ActionChains(self.driver).move_to_element(first_product).perform()
+
+
+
+        add_buttons = self.finds(self.ADD_TO_CART_BTNS)
+
+        first_add_btn = add_buttons[0]
+
+
+
+        # javascript click (more stable in CI)
+
+        self.driver.execute_script(
+
+            "arguments[0].click();",
+
+            first_add_btn
+
+        )
+
+
+
+    # click view cart popup button
+
+    def view_cart(self):
+
+        self.wait_for(self.VIEW_CART_BTN)
+
+        view_cart_btn = self.find(self.VIEW_CART_BTN)
+
+
+
+        self.driver.execute_script(
+
+            "arguments[0].click();",
+
+            view_cart_btn
+
+        )

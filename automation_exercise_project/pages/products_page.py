4bb88db4af72277f9f5e5c10fb681a80b94cfcb6
nitish902 <nitishshukla902@gmail.com>
2026-04-02 12:44:43 +0530

    Update products_page.py

--- automation_exercise_project/pages/products_page.py
+++ automation_exercise_project/pages/products_page.py
@@ -1,19 +1,20 @@
 from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
 
 
 class ProductsPage(BasePage):
 
     PRODUCTS_TITLE = "//h2[text()='All Products']"
 
-    PRODUCT_NAMES = "//div[@class='productinfo text-center']/p"
+    PRODUCT_CARDS = "//div[@class='product-image-wrapper']"
 
-    PRODUCT_LINKS = "//div[@class='productinfo text-center']/p/../a"
+    PRODUCT_NAMES = "//div[@class='productinfo text-center']/p"
 
     SEARCH_INPUT = "//input[@id='search_product']"
 
     SEARCH_BTN = "//button[@id='submit_search']"
 
-    ADD_TO_CART_BTN_PRODUCT_PAGE = "//button[contains(text(),'Add to cart')]"
+    ADD_TO_CART_BTN = "(//a[contains(text(),'Add to cart')])[1]"
 
 
 
@@ -25,7 +26,7 @@ class ProductsPage(BasePage):
 
     def get_product_count(self):
 
-        return len(self.finds(self.PRODUCT_LINKS))
+        return len(self.finds(self.PRODUCT_CARDS))
 
 
 
@@ -45,114 +46,23 @@ class ProductsPage(BasePage):
 
 
 
-    # open first product page
+    # stable add to cart
 
-    def open_first_product(self):
+    def add_first_product_to_cart(self):
 
-        products = self.finds(self.PRODUCT_LINKS)
-
-        first_product = products[0]
+        add_btn = self.find(self.ADD_TO_CART_BTN)
 
         self.driver.execute_script(
 
             "arguments[0].click();",
 
-            first_product
+            add_btn
 
         )
 
 
 
-    # add to cart from product page
-
-    def add_first_product_to_cart(self):
-
-        self.open_first_product()
-
-        self.click(self.ADD_TO_CART_BTN_PRODUCT_PAGE)
-
-
-
-    # open cart page directly
-
-    def view_cart(self):
-
-        self.driver.get("https://automationexercise.com/view_cart")from pages.base_page import BasePage
-
-
-class ProductsPage(BasePage):
-
-    PRODUCTS_TITLE = "//h2[text()='All Products']"
-
-    PRODUCT_NAMES = "//div[@class='productinfo text-center']/p"
-
-    PRODUCT_LINKS = "//div[@class='productinfo text-center']/p/../a"
-
-    SEARCH_INPUT = "//input[@id='search_product']"
-
-    SEARCH_BTN = "//button[@id='submit_search']"
-
-    ADD_TO_CART_BTN_PRODUCT_PAGE = "//button[contains(text(),'Add to cart')]"
-
-
-
-    def is_products_page_loaded(self):
-
-        return self.find(self.PRODUCTS_TITLE).is_displayed()
-
-
-
-    def get_product_count(self):
-
-        return len(self.finds(self.PRODUCT_LINKS))
-
-
-
-    def get_all_product_names(self):
-
-        elements = self.finds(self.PRODUCT_NAMES)
-
-        return [e.text for e in elements]
-
-
-
-    def search_product(self, product_name):
-
-        self.type(self.SEARCH_INPUT, product_name)
-
-        self.click(self.SEARCH_BTN)
-
-
-
-    # open first product page
-
-    def open_first_product(self):
-
-        products = self.finds(self.PRODUCT_LINKS)
-
-        first_product = products[0]
-
-        self.driver.execute_script(
-
-            "arguments[0].click();",
-
-            first_product
-
-        )
-
-
-
-    # add to cart from product page
-
-    def add_first_product_to_cart(self):
-
-        self.open_first_product()
-
-        self.click(self.ADD_TO_CART_BTN_PRODUCT_PAGE)
-
-
-
-    # open cart page directly
+    # stable cart navigation
 
     def view_cart(self):
 
